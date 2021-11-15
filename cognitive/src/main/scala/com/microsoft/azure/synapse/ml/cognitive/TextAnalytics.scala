5fca58905bc74e32e07197e82162758d6bb22f29
Stuart Leeks <stuartle@microsoft.com>
2021-11-15 16:18:47 +0000

    wip

--- cognitive/src/main/scala/com/microsoft/azure/synapse/ml/cognitive/TextAnalytics.scala
+++ cognitive/src/main/scala/com/microsoft/azure/synapse/ml/cognitive/TextAnalytics.scala
@@ -532,9 +532,10 @@ val text = new ServiceParam[Seq[String]](this, "text", "the text in the request 
         val doc = entityRecognitionTaskResultsDocuments(0)
 
         val entityRecognitionRow = Row.fromSeq(Seq(doc, None))
-        val taaResult = Seq(entityRecognitionRow, Row.fromSeq(Seq("bar", None)))
+        val taaResult = Seq(entityRecognitionRow, None)
+        // val taaResult = Seq(entityRecognitionRow, Row.fromSeq(Seq("bar", None)))
         val resultRow = Row.fromSeq(taaResult)
-        val results = Seq(resultRow)
+        val results = Seq(resultRow) // element per input row (i.e. per document)
         results
       }
     // }, innerResponseDataType
